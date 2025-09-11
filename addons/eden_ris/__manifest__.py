d2426470efa3ac67a717c2a3d561e9a25f813102
Jorge Juarez <contacto@jorgejuarez.net>
2025-09-11 02:51:28 +0000

    fix: settings

--- addons/eden_ris/__manifest__.py
+++ addons/eden_ris/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": "Short (1 phrase/line) summary of the module's purpose",
     "description": "Long description of module's purpose",
     "author": "My Company",
-    "website": "https://www.yourcompany.com",
+    "website": "https://www.jorgejuarez.net",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
--- addons/eden_ris/controllers/controllers.py
+++ addons/eden_ris/controllers/controllers.py
@@ -37,7 +37,6 @@ _log = logging.getLogger(__name__)
 #         return {"status": "ok"}
 
 
-
 def custom_api_key_auth(func):
     def wrapper(*args, **kwargs):
         # api_key = http.request.httprequest.headers.get('X-API-Key')
@@ -46,26 +45,34 @@ def custom_api_key_auth(func):
         #     return func(*args, **kwargs)
         # else:
         #     return http.Response("Unauthorized", status=401)
-        http.request.session.authenticate(
-            "odoo_ris", "admin", "admin"
-        )
+        http.request.session.authenticate("odoo_ris", "admin", "admin")
         return func(*args, **kwargs)
+
     return wrapper
 
 
 class EdenRis(http.Controller):
-    @http.route("/api/v1/hl7/siu/", auth="none", type="json", csrf=False, methods=["POST"])
+    @http.route(
+        "/api/v1/hl7/siu/", auth="none", type="json", csrf=False, methods=["POST"]
+    )
     @custom_api_key_auth
     def api_siu(self, **kw):
         env = http.request.env
         data = http.request.get_json_data()
-        env["sale.order"].create({
-            "name": data["data"]["folio"],
-            "partner_id": env.ref("base.res_partner_1").id,
-            "order_line": [(0, 0, {
-                "name": data["data"]["description"],
-                "product_id": env.ref("product.product_product_1").id
-            })]
-        })
+        env["sale.order"].create(
+            {
+                "name": data["data"]["folio"],
+                "partner_id": env.ref("base.res_partner_1").id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": data["data"]["description"],
+                            "product_id": env.ref("product.product_product_1").id,
+                        },
+                    )
+                ],
+            }
+        )
         return http.Response("OK", status=400)
-    
