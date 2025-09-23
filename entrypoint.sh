f62e89de8a6bf146a800d1033b32338de554cd23
Jorge Juarez <contacto@jorgejuarez.net>
2025-09-23 05:10:03 +0000

    fix: workers

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,27 +29,29 @@ check_config "db_host" "$HOST"
 check_config "db_port" "$PORT"
 check_config "db_user" "$USER"
 check_config "db_password" "$PASSWORD"
-check_config "load" "$ODOO_LOAD_MODULES"
-check_config "workers" "$ODOO_WORKERS"
+# check_config "load" "$ODOO_LOAD_MODULES"
+# check_config "workers" "$ODOO_WORKERS"
 
-# case "$1" in
-#     -- | odoo)
-#         shift
-#         if [[ "$1" == "scaffold" ]] ; then
-#             exec odoo "$@"
-#         else
-#             wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-#             exec odoo "$@" "${DB_ARGS[@]}"
-#         fi
-#         ;;
-#     -*)
-#         wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-#         exec odoo "$@" "${DB_ARGS[@]}"
-#         ;;
-#     *)
-#         exec "$@"
-# esac
+EXTRA_ARGS=()
+EXTRA_ARGS+=("--load=$ODOO_LOAD_MODULES")
+EXTRA_ARGS+=("--workers=$ODOO_WORKERS")
 
-# exit 1
+case "$1" in
+    -- | odoo)
+        shift
+        if [[ "$1" == "scaffold" ]] ; then
+            exec odoo "$@"
+        else
+            wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+            exec odoo "$@" "${DB_ARGS[@]}" "${EXTRA_ARGS[@]}"
+        fi
+        ;;
+    -*)
+        wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+        exec odoo "$@" "${DB_ARGS[@]}" "${EXTRA_ARGS[@]}"
+        ;;
+    *)
+        exec "$@"
+esac
 
-echo ${DB_ARGS[@]}
+exit 1
